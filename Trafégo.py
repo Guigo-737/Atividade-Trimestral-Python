bc5c353fc837d05d5a29d7f7d6294ac06ec1fdcb
Guigo-737 <rodrigohernandes@colegiopaulodetarso.com.br>
2025-04-14 15:15:28 +0000

    fora exu

--- Trafégo.py
+++ Trafégo.py
@@ -3,7 +3,14 @@ import os
 import time
 from datetime import datetime
 
-# Lista das ruas
+
+#▒▒▒▒▒▒▒▒▒▒▒▒
+#▒▒▒▒▓▒▒▓▒▒▒▒
+#▒▒▒▒▓▒▒▓▒▒▒▒
+#▒▒▒▒▒▒▒▒▒▒▒▒
+#▒▓▒▒▒▒▒▒▒▒▓▒
+#▒▒▓▓▓▓▓▓▓▓▒▒
+#▒▒▒▒▒▒▒▒▒▒▒▒
 ruas = [
     "Rua das Palmeiras Douradas",
     "Avenida do Sol Nascente",
@@ -17,43 +24,44 @@ ruas = [
     "Rua do Vento Suave"
 ]
 
-# Lista de status
+#─▄▀▀▀▄────▄▀█▀▀█▄
+#▄▀─▀─▀▄▄▀█▄▀─▄▀─▄▀▄
+#█▄▀█───█─█▄▄▀─▄▀─▄▀▄
+#──█▄▄▀▀█▄─▀▀▀▀▀▀▀─▄█
+#─────▄████▀▀▀▀████─▀▄
 status_opcoes = ["Livre", "Movimentado", "Cheio"]
 
 try:
     while True:
-        # Embaralha as ruas para não repetir dentro do mesmo ciclo
+
         ruas_embaralhadas = ruas.copy()
         random.shuffle(ruas_embaralhadas)
 
         for rua in ruas_embaralhadas:
-            # Limpa a tela
+
             os.system('cls' if os.name == 'nt' else 'clear')
 
-            # Pega o horário atual (HH:MM)
             agora = datetime.now()
             horario_formatado = agora.strftime("%H:%M")
 
-            # Escolhe status aleatoriamente (pode repetir)
             status = random.choice(status_opcoes)
 
-            # Exibe informações
+# ░░░░░░███████ ]▄▄▄▄▄▄▄▄▃
+#▂▄▅█████████▅▄▃▂
+#I███████████████████].
+#◥⊙▲⊙▲⊙▲⊙▲⊙▲⊙▲⊙◤
+
+
             print(f"Horário: {horario_formatado}")
             print(f"Rua: {rua}")
             print(f"Status: {status}")
 
-            # Espera 3 segundos antes de mostrar a próxima
+# ───▄██▄─██▄───▄
+#─▄██████████▄███▄
+#─▌████████████▌
+#▐▐█░█▌░▀████▀░░
+#░▐▄▐▄░░░▐▄▐▄░░░░
             time.sleep(10)
 
-# ░░░░░░░░░░░█▀▀░░█░░░░░░
-# ░░░░░░▄▀▀▀▀░░░░░█▄▄░░░░
-# ░░░░░░█░█░░░░░░░░░░▐░░░
-# ░░░░░░▐▐░░░░░░░░░▄░▐░░░
-# ░░░░░░█░░░░░░░░▄▀▀░▐░░░
-# ░░░░▄▀░░░░░░░░▐░▄▄▀░░░░
-# ░░▄▀░░░▐░░░░░█▄▀░▐░░░░░
-# ░░█░░░▐░░░░░░░░▄░█░░░░░
-# ░░░█▄░░▀▄░░░░▄▀▐░█░░░░░
-# ░░░█▐▀▀▀░▀▀▀▀░░▐░█░░░░░
-# ░░▐█▐▄░░▀░░░░░░▐░█▄▄░░
-# ░░░▀▀░▄███▄▄░░░▐▄▄▄▀░░░ 
+except KeyboardInterrupt:
+    print("\nExecução interrompida.")
