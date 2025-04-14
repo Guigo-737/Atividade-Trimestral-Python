7471c48ed22ae965763557b45fe4a6e42992fc5c
Guigo-737 <rodrigohernandes@colegiopaulodetarso.com.br>
2025-04-14 15:54:52 +0000

    mojang meu ovo

--- Vendas.py
+++ Vendas.py
@@ -6,7 +6,8 @@ import random
 def lucro():
     while True:
         lucro = random.uniform(1000, 2500, 3900, 5200)
-        print(f"o lucro do mês {mês:.2f} foi de {lucro:.2f} ")
+        print(f"o lucro do mês {mês:.2f}")
+        print("foi de {lucro:.2f} ao todo")      
         mês = random.uniform(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12)
 
         time.sleep(5)
