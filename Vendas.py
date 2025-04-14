1f9e4a156f5c4fdf8abd268977beb99ee0936af7
Guigo-737 <rodrigohernandes@colegiopaulodetarso.com.br>
2025-04-14 15:51:56 +0000

    macumba do ruim

---
+++ Vendas.py
@@ -0,0 +1,12 @@
+import time
+import random
+
+
+
+def lucro():
+    while True:
+        lucro = random.uniform(1000, 2500, 3900, 5200)
+        print(f"o lucro do mês {mês:.2f} foi de {lucro:.2f} ")
+        mês = random.uniform(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12)
+
+        time.sleep(5)
