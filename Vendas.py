0438fd1a8c04d0b39c07403ce5c9278d26d46432
Guigo-737 <rodrigohernandes@colegiopaulodetarso.com.br>
2025-04-15 02:41:41 +0000

    aba

--- Vendas.py
+++ Vendas.py
@@ -5,9 +5,12 @@ import random
 
 def lucro():
     while True:
-        lucro = random.uniform(1000, 2500, 3900, 5200)
-        print(f"o lucro do mês {mês:.2f}")
-        print("foi de {lucro:.2f} ao todo")      
-        mês = random.uniform(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12)
+        lucro = random.uniform(1000, 5000)      
+
+        
+        mês = random.randint(1, 12)
+
+        print("")
+        print(f"O lucro do mês {mês} foi de R${lucro:.2f} ao todo.")
 
         time.sleep(5)
