004c956a219a7b8e76aeaf15f38e54d80b20a19d
Guigo-737 <rodrigohernandes@colegiopaulodetarso.com.br>
2025-04-15 16:15:01 +0000

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- Vendas.py
+++ Vendas.py
@@ -1,16 +1,10 @@
 import time
 import random
 
-
-
-def lucro():
-    while True:
-        lucro = random.uniform(1000, 5000)      
-
-        
-        mês = random.randint(1, 12)
-
-        print("")
-        print(f"O lucro do mês {mês} foi de R${lucro:.2f} ao todo.")
-
+def simular_lucro(qtd_meses=12):
+    for mes in range(1, qtd_meses + 1):
+        valor_lucro = random.uniform(-1000, 5000)
+        print("———————————————————————————————————————————————————")
+        print(f"O lucro do mês {mes} foi de R${valor_lucro:.2f} ao todo.")
+        print("———————————————————————————————————————————————————")
         time.sleep(5)
